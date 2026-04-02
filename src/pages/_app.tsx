93b7ab5c56b222d54686429e2642ca3c2d93d377
오청열 <ocy@nis.co.kr>
2026-04-02 11:06:48 +0900

    라우팅 방법

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push('/test');
+
+  }
+
   return (
   <>
-    <div>header</div>
+    <header>
+      <Link href={"/"}>home</Link>
+      &nbsp;
+      <Link href={"/search"}>search</Link>
+      &nbsp;
+      <Link href={"/book/1"}>book/1</Link>
+      &nbsp;
+      <button onClick={onClickButton}>test 이동</button>
+    </header>
     <Component {...pageProps} />
   </>);
 }
---
+++ src/pages/book/[id].tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+
+export default function Page(){
+    const router = useRouter();
+    console.log(router);
+    const {id} = router.query;
+
+    return <h1>book {id}</h1>;
+
+}
