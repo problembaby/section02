2873f4c0d45a5dc13a79c612da917e5b9063ac7d
오청열 <ocy@nis.co.kr>
2026-04-02 11:17:45 +0900

    프리패칭

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -12,12 +13,16 @@ export default function App({ Component, pageProps }: AppProps) {
 
   }
 
+  useEffect(() => {
+    router.prefetch('/test');
+  },[]);
+
   return (
   <>
     <header>
       <Link href={"/"}>home</Link>
       &nbsp;
-      <Link href={"/search"}>search</Link>
+      <Link href={"/search"} prefetch={false}>search</Link>
       &nbsp;
       <Link href={"/book/1"}>book/1</Link>
       &nbsp;
